bda5d6ee17108efe291f9b2080e7684c20cf03e2
levina <82658782+levina-lab@users.noreply.github.com>
2022-02-12 19:58:54 +0700

    rename

--- config.py
+++ config.py
@@ -18,8 +18,8 @@ BOT_USERNAME = getenv("BOT_USERNAME")
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/levina-lab/video-stream")
 UPSTREAM_BRANCH = getenv("UPSTREM_BRANCH", "main")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "60"))
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "VeezSupportGroup")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "levinachannel")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "VeezSupportGroup")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "levinachannel")
 
 # database, decorators, handlers mandatory vars
 MONGODB_URL = getenv("MONGODB_URL")
